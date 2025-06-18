4ee31f154945a5511928b16c74440a04f709e44a
Prityush Parmar <89090802+prityushsingh@users.noreply.github.com>
2025-06-18 13:55:34 +0530

    :hammer: [examples/face_detection] Add --image_path CLI argument to basic_detection.py (#15)
    * Add CLI argument to specify image path in basic_detection.py
    
    * Update usage docstring as requested
    
    Update usage docstring as requested
    
    ---------
    
    Co-authored-by: Ishan Dutta <ishandutta0098@gmail.com>

--- examples/face_detection/basic_detection.py
+++ examples/face_detection/basic_detection.py
@@ -2,8 +2,11 @@
 Basic example showing how to use a single face detector to detect faces in an image.
 
 Usage:
-python examples/face_detection/basic_detection.py --detection_model mediapipe
+python examples/face_detection/basic_detection.py \
+    --detection_model mediapipe \
+    --image_path assets/images/img1.jpg
 """
+
 
 import argparse
 
@@ -17,6 +20,12 @@ parser.add_argument(
     default="ultralight",
     help="Choose the face detection model to use.",
 )
+parser.add_argument(
+    "--image_path",
+    type=str,
+    default="assets/images/img1.jpg",
+    help="Path to the image to detect faces in.",
+)
 
 args = parser.parse_args()
 
@@ -24,7 +33,7 @@ args = parser.parse_args()
 detector = FaceDetector.create(args.detection_model)
 
 detections = detector.detect(
-    image_path="assets/images/img1.jpg",  # Path to the image to detect faces in
+    image_path=args.image_path,  # Path to the image to detect faces in
     save_csv=True,  # Save the detections to a CSV file
     csv_path=f"output/{args.detection_model}/detections.csv",  # Path to save the CSV file
     save_annotated=True,  # Save the annotated image
